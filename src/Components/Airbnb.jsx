5fc11d68fb99520e47c0f279f523d2967fcbffc7
Anurag Nayajk <anuragnayak345@gmail.com>
2022-11-12 01:32:29 +0530

    initial commit

---
+++ src/Components/Airbnb.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const Airbnb = () => {
+  return (
+    <>
+      <div className="main">
+        <h1>The Evolution Of Airbnb's Frontend</h1>
+        <h3>@spikebrehm</h3>
+        <img src={require("../Images/airbnb_l.png")} alt="img" />
+      </div>
+    </>
+  );
+};
+
+export default Airbnb;
